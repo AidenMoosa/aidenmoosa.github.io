1a681e8feeb10e9772bea0ee7aa0490db67b3c3d
Aiden Moosa <aidenmoosa@Aidens-MacBook-Pro.local>
2019-08-15 22:15:52 +0100

    Added p5js stuff

--- sketch.js
+++ sketch.js
@@ -1,9 +1,28 @@
+var canvas;
+var paintCanvas;
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function setup() {
-  createCanvas(710, 400, WEBGL);
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+
+  paintCanvas = createGraphics(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(250);
+  background(0);
+
+  imageMode(CENTER);
+  if (mouseIsPressed) {
+    paintCanvas.stroke(255);
+    paintCanvas.line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+  image(paintCanvas, 0, 0);
+
 
   translate(-240, -100, 0);
   normalMaterial();
@@ -38,7 +57,7 @@ function draw() {
   cone(70, 70);
   pop();
 
-  translate(240, 0, 0);
+  translate(240 + 60*sin(millis()*0.001), 240*cos(millis()*0.001),240*cos(millis()*0.001));
   push();
   rotateZ(frameCount * 0.01);
   rotateX(frameCount * 0.01);
